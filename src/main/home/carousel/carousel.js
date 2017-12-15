669b37a029eeb4353779172a75b4daca7322eae2
alexandr <Subwan@gmail.com>
2017-12-15 17:16:56 +0300

    Rewrite carousel to react-slick.

--- src/main/home/carousel/carousel.js
+++ src/main/home/carousel/carousel.js
@@ -1,53 +1,89 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 import CarouselItems from './items.js';
+
 import './carousel.css';
 
 function ItemList(props) {
   const item = props.item;
   return (
     <div className="item">
-      <img className="first-slide" src={item.src} alt="Slide" />
-      <div className="container">
-        <div className="carousel-caption">
-          <h1>{item.head}</h1>
-          <p>{item.text}</p>
-        </div>
+      <img src={item.src} alt="Slide" />
+      <div className="item__caption">
+        <h1>{item.head}</h1>
+        <p>{item.text}</p>
       </div>
     </div>
   );
 }
 
+function SampleNextArrow(props) {
+  const {className, style, onClick} = props
+  return (
+    <div
+      className={className}
+      style={{...style}}
+      onClick={onClick}
+    ></div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const {className, style, onClick} = props
+  return (
+    <div
+      className={className}
+      style={{...style}}
+      onClick={onClick}
+    ></div>
+  );
+}
 
 class Carousel extends React.Component {
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
+    }
     const items = CarouselItems;
     const itemList = items.map((item) =>
-      <ItemList item={item} />
+      <div> <ItemList item={item} /> </div>
     );
     return (
-      <div id="banner" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#banner" data-slide-to="0" className="active"></li>
-          <li data-target="#banner" data-slide-to="1"></li>
-          <li data-target="#banner" data-slide-to="2"></li>
-          <li data-target="#banner" data-slide-to="3"></li>
-          <li data-target="#banner" data-slide-to="4"></li>
-        </ol>
-        <div className="carousel-inner" role="listbox">
+      <div className="carousel">
+        <Slider {...settings}>
           {itemList}
-        </div>
-        <div className="center-align">
-          <a className="left carousel-control" href="#banner" role="button" data-slide="prev">
-            <span className="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="right carousel-control" href="#banner" role="true" data-slide="next">
-            <span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
+        </Slider>
       </div>
+
+      // <div id="banner" className="carousel slide" data-ride="carousel">
+      //   <ol className="carousel-indicators">
+      //     <li data-target="#banner" data-slide-to="0" className="active"></li>
+      //     <li data-target="#banner" data-slide-to="1"></li>
+      //     <li data-target="#banner" data-slide-to="2"></li>
+      //     <li data-target="#banner" data-slide-to="3"></li>
+      //     <li data-target="#banner" data-slide-to="4"></li>
+      //   </ol>
+      //   <div className="carousel-inner" role="listbox">
+      //     {itemList}
+      //   </div>
+      //   <div className="center-align">
+      //     <a className="left carousel-control" href="#banner" role="button" data-slide="prev">
+      //       <span className="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+      //       <span className="sr-only">Previous</span>
+      //     </a>
+      //     <a className="right carousel-control" href="#banner" role="true" data-slide="next">
+      //       <span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+      //       <span className="sr-only">Next</span>
+      //     </a>
+      //   </div>
+      // </div>
     );
   }
 };
--- src/main/home/carousel/items.js
+++ src/main/home/carousel/items.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-
+import banner from '../../../assets/banner.png';
 
 const CarouselItems = [
-    {src: "../../../assets/banner.png", head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the <br> industry's standard dummy text ever since."},
-    {src: "../../../assets/banner.png", head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the <br> industry's standard dummy text ever since."},
-    {src: "../../../assets/banner.png", head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the <br> industry's standard dummy text ever since."},
-    {src: "../../../assets/banner.png", head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the <br> industry's standard dummy text ever since."},
-    {src: "../../../assets/banner.png", head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the <br> industry's standard dummy text ever since."}  
+    {src: banner, head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since."},
+    {src: banner, head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since."},
+    {src: banner, head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since."},
+    {src: banner, head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since."},
+    {src: banner, head: "WELCOM TO OUR WEB SITE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since."}  
 ]
 
 export default CarouselItems;
